2fb1fe2bab61c576138c9993ffa1a3c2b9507254
jh <dev1@correo.com>
2019-04-29 18:35:59 -0500

    subiida al servidor db

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose')
 
 //connecting to the db, useNewUrlParser is an extra setting from mongoose in order to avoid futere errors, it is a promise, so once connected log DB connected
-mongoose.connect('mongodb://localhost/finterest',{
+mongoose.connect('mongodb+srv://jhgutsol:jhgutsol@cluster0-ygswu.mongodb.net/test?retryWrites=true',{
     useNewUrlParser: true
 })
     .then(db=>console.log(`DB connected`))
